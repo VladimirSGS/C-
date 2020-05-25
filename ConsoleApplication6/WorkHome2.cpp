44ee684c73a24b3cd91cf2eccd697deb5d481c8c
Владимир <vladimir.sgs@gmail.com>
2020-05-25 20:24:14 +0300

    Напишите программу, проверяющую число, введнное с клавиатуры на четность.

---
+++ ConsoleApplication6/WorkHome2.cpp
@@ -0,0 +1,21 @@
+﻿
+// Напишите программу, проверяющую число, введнное с клавиатуры на четность.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int number;
+	cin << number;
+	if (number % 2 == 0) {
+		cout << "Число " << number <<"четное";
+	}
+	else {
+		cout << "Число " << number <<"не четное";
+	}
+}
+
